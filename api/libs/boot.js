86e7cab5c35787c402ca4c913abbec9f7c86b50d
JAreina <jareinafdez@gmail.com>
2017-12-18 19:05:48 +0100

    funciona

--- api/libs/boot.js
+++
@@ -1,5 +0,0 @@
-module.exports=app=>{
-    app.listen(app.get("port"),()=>{
-        console.log(`EXPRESS FUNCIONA en PUERTO :${app.get("port")}`)
-    }) 
-}
--- api/libs/middlewares.js
+++
@@ -1,5 +0,0 @@
-module.exports = app =>{
-    //formatear la salida json en navegador
-    app.set("json spaces",6);
-    app.set("port",2222);
-}
